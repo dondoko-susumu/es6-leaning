93841ba54ac7fe4464252ae16291c8650bd8ee71
kubota <kkubota@jlce.jp>
2015-09-04 09:32:14 +0900

    first commit

---
+++ babel/es5/arrow.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var add = function add(a, b) {
+	return a + b;
+};
+
+// ブロックの波括弧とreturnを省略
+var add = function add(a, b) {
+	return a + b;
+};
+
+// 引数の丸括弧を省略
+var square = function square(n) {
+	return n * n;
+};
+
---
+++ babel/es6/arrow.js
@@ -0,0 +1,10 @@
+var add = (a,b) => {
+	return a + b;
+};
+
+// ブロックの波括弧とreturnを省略
+var add = (a,b) => a + b;
+
+// 引数の丸括弧を省略
+var square = n => n * n;
+
